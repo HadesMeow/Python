b84f2e88d3714f933bb273eed9d23fb4dc4eb94e
HadesMeow <369022182@qq.com>
2020-05-27 22:34:17 +0800

    add Game ticTacToe.py

---
+++ characterCount.py
@@ -0,0 +1,9 @@
+import pprint
+message = 'It was a bright cold day in April, and the clocks were striking thireen.'
+count ={}
+
+for characters in message:
+    count.setdefault(characters,0)
+    count[characters] = count[characters] + 1
+    
+pprint.pprint(count)
